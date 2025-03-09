f479d0df3324c051f5869c85b036d04f4ecb48e0
Drew-Levy <dslevy@uci.edu>
2025-03-09 20:29:21 +0000

    fix

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -38,10 +38,9 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 }
 
 vector<int> extract_shortest_path(const vector<int>& distances,const vector<int>& previous, int destination) {
-      if (destination < 0 ||  static_cast<size_t>(destination) >= distances.size() || distances[destination] == INF) {
-        //return {}; 
+      if (destination < 0 ||  static_cast<size_t>(destination) >= distances.size() || distances[destination] == -1) {
+        return {}; 
     }
-    
     vector<int> path;
     for (int v = destination; v != -1; v = previous[v]) {
         path.push_back(v);
@@ -52,8 +51,8 @@ vector<int> extract_shortest_path(const vector<int>& distances,const vector<int>
 
 void print_path(const vector<int>& v, int total){
     for(size_t i =0; i  < v.size(); ++i){
-        cout << v[i];
-        if( i !=v.size()) cout << " ";
+        cout << v[i] << " ";
+        //if( i !=v.size()) cout << " ";
     }
     cout << "\nTotal cost is " << total << "\n";
 }
